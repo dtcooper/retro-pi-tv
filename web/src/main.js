13555c3dd2e5ac3b264941fa375a15b216bfc3ee
David Cooper <david@dtcooper.com>
2024-04-15 16:07:31 -0400

    Bump deps

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,8 @@ import App from "./App.svelte"
 import "./app.css"
 
 const app = new App({
-  target: document.getElementById("app")
+  target: document.getElementById("app"),
+  props: {}
 })
 
 export default app
